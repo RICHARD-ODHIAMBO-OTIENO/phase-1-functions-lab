90a89dd6a3e087908a56b19760f54b6724278640
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-03-28 07:24:59 +0300

    Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+// Createa function called distanceFromHqInBlocks
+function distanceFromHqInBlocks(Pickuplocation) {
+  return Math.abs(Pickuplocation - 42); 
+}
+
+// Function calculates the distance in feet from Scuber's HQ to the pickup location
+function distanceFromHqInFeet(pickuplocation) {
+    return distanceFromHqInBlocks(pickuplocation) * 264;
+}
+
+//Function to calculate the distance travelled
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+//Function to calculate fare price for a trip
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination );
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
+}
